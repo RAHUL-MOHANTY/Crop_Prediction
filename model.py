b7168d64206e9203fa7d5366fd3f1e9006f204ae
RAHUL-MOHANTY <mohantyrahul257@gmail.com>
2025-11-09 18:21:01 +0530

    Update model.py

--- model.py
+++ model.py
@@ -28,8 +28,3 @@ pickle.dump(model, open("model.pkl", "wb"))
 # Evaluate the model
 # accuracy = model.score(X_test, y_test)
 # print("Accuracy:", accuracy)
-
-# Example usage: Predict crop for a new set of features
-# new_features = [[117 ,32,34,26.2724184,52.12739421,6.758792552,127.1752928,]]  # Replace with your own set of features
-# predicted_crop = model.predict(new_features)
-# print("Predicted crop:", predicted_crop)
